6f35d264adef2fd7e0e87a0150ecb380f2a6d699
Tony Flury <anthony.flury@btinternet.com>
2016-01-10 12:12:18 +0000

    Changes to Support collectstatic

--- cyclists/apps.py
+++ cyclists/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class RegisterConfig(AppConfig):
+class cyclistsConfig(AppConfig):
     name = 'cyclists'
--- newsletter/apps.py
+++ newsletter/apps.py
@@ -1,7 +1,5 @@
-from __future__ import unicode_literals
-
 from django.apps import AppConfig
 
 
-class NewsletterConfig(AppConfig):
+class newsletterConfig(AppConfig):
     name = 'newsletter'
