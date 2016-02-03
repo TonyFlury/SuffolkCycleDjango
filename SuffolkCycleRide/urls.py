1e1fdce6ea7b23a008731401d93164f893165254
Tony Flury <anthony.flury@btinternet.com>
2016-02-03 01:34:23 +0000

    Updated Blog Implementation. Now have : Better Archive list, search on Year & Month. Pagination, better formatting. Sub heading to indicate searches.

--- SuffolkCycleRide/urls.py
+++ SuffolkCycleRide/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -11,6 +12,7 @@ urlpatterns = [
     url(r'^$', home, name='Home'),
     url(r'^readmore$',readmore, name='Readmore'),
     url(r'^getinvolved$', GetInvolved.as_view(), name="GetInvolved"),
+    url(r'^technology$', TemplateView.as_view(template_name='SuffolkCycleRide/pages/technology.html') ),
     url(r'^RegisteredUsers/', include("RegisteredUsers.urls", namespace='User')),
     url(r'^newsletter/', include("newsletter.urls", namespace="newsletter") ),
     url(r'^dashboard/', include("dashboard.urls", namespace="Dashboard")),
