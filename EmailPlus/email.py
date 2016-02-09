9d3f0ca51ffbce570869303cb419804abb206b57
Tony Flury <anthony.flury@btinternet.com>
2016-02-09 14:57:53 +0000

    Issue 8 : Registration fails with Server 500 error when smtp error fails.

--- EmailPlus/email.py
+++ EmailPlus/email.py
@@ -18,6 +18,7 @@ from django.conf import settings
 from models import EmailQueue
 from django.utils.timezone import now
 import scheduler
+import logging
 
 __version__ = "0.1"
 __author__ = 'Tony Flury : anthony.flury@btinternet.com'
@@ -35,15 +36,18 @@ class Email(object):
     def send(self, destination_email):
         """ Try to send the email immediately if we are able"""
         if (scheduler._scheduler_inst is None) or (scheduler._scheduler_inst.immediate_dispatch()):
-            send_mail(subject=self._subject,
-                      from_email=settings.DEFAULT_FROM_EMAIL,
-                      recipient_list=[destination_email, settings.DEFAULT_FROM_EMAIL],
-                      message=self._body)
-            # Even if we can send immediately - we still need to record so that the volume sent is recorded.
-            if scheduler._scheduler_inst is not None:
-                qe = EmailQueue.objects.create(subject=self._subject, body_text=self._body,
-                                               destination=destination_email,
-                                               sent=True, sent_time=now(), queue_time=now())
+            try:
+                send_mail(subject=self._subject,
+                          from_email=settings.DEFAULT_FROM_EMAIL,
+                          recipient_list=[destination_email, settings.DEFAULT_FROM_EMAIL],
+                          message=self._body)
+                # Even if we can send immediately - we still need to record so that the volume sent is recorded.
+                if scheduler._scheduler_inst is not None:
+                    qe = EmailQueue.objects.create(subject=self._subject, body_text=self._body,
+                                                   destination=destination_email,
+                                                   sent=True, sent_time=now(), queue_time=now())
+            except BaseException as e:
+                logging.error('Error sending email to {} : {}'.format(destination_email, e))
         else:
             qe = EmailQueue.objects.create(subject=self._subject, body_text=self._body, destination=destination_email,
                                            sent=False, queue_time=now())
