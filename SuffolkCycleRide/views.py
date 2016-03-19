7f9db30ac8594ac6e122f2674c4e921e0304a601
Tony Flury <anthony.flury@btinternet.com>
2016-03-19 22:18:16 +0000

    Incorrect emails from ContactUs page

--- SuffolkCycleRide/views.py
+++ SuffolkCycleRide/views.py
@@ -157,7 +157,7 @@ class ContactUs(View):
                        from_email = settings.DEFAULT_FROM_EMAIL,
                        recipient_list = [settings.DEFAULT_TO_EMAIL],
                        message = render_to_string(
-                               'SuffolkCycleRide/emails/ContactUs_ToUser',
+                               'SuffolkCycleRide/emails/ContactUs_ToStaff',
                                context= dict( {'host':settings.BASE_URL,
                                             'reason_text': forms.ContactChoices.fullVersion(data['reason'])}.items() +\
                                             data.items() ) ))
@@ -167,7 +167,7 @@ class ContactUs(View):
                        from_email = settings.DEFAULT_FROM_EMAIL,
                        recipient_list = [data['sender_email']],
                        message =render_to_string(
-                               'SuffolkCycleRide/emails/ContactUs_ToStaff',
+                               'SuffolkCycleRide/emails/ContactUs_ToUser',
                                context = dict( {'host':settings.BASE_URL,
                                             'reason_text' :forms.ContactChoices.fullVersion(data['reason'])}.items() +\
                                             data.items() ) ) )
